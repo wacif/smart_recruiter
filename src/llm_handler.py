5f632060cebe445b6b3811b45494aaac8f8b044f
Wasif Nawaz <99590687+wacif@users.noreply.github.com>
2024-10-31 20:56:25 +0500

    Update llm_handler.py

--- src/llm_handler.py
+++ src/llm_handler.py
@@ -9,7 +9,7 @@ api_key = st.secrets["GROQ_API_KEY"]
 client = Groq(
     api_key=api_key)
 
-def call_groq_api(messages, model="llama3-8b-8192"):
+def call_groq_api(messages, model="llama-3.2-90b-vision-preview"):
     """Calls Groq API to generate a response based on input messages."""
     response = client.chat.completions.create(
         messages=messages,
